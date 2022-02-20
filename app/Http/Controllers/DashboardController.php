453fa9974b534496f9759164d6c701470342003d
ahmed_shaan <iahmednill@gmail.com>
2022-02-20 23:14:02 +0500

    - dping stuff

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -28,7 +28,29 @@ class DashboardController extends Controller
 //        $covid_first = Vaccination::where('dose','first')->where('vaccine_id', 6)->count();
 //        $covid_second = Vaccination::where('dose','second')->where('vaccine_id', 6)->count();
 
-        $vaccinations = Vac
+        $vaccinations = Vaccination::query()
+//            ->with('vaccine:id,brand')
+            ->select('id', 'vaccine_id', 'dose')
+            ->get();
+
+        $first_dose = $vaccinations->where('dose', 'first')->count();
+        $second_dose = $vaccinations->where('dose', 'second')->count();
+        $booster_dose = $vaccinations->where('dose', 'booster')->count();
+
+        $total_eligible = config('defaults.total_eligible');
+        $first_dose_percentage = $first_dose / $total_eligible * 100;
+        $second_dose_percentage = $second_dose / $total_eligible * 100;
+        $booster_dose_percentage = $booster_dose / $total_eligible * 100;
+
+        $pfizer_first = $vaccinations->where('dose', 'first')->where('vaccine_id', 2)->count();
+        $pfizer_second = $vaccinations->where('dose', 'second')->where('vaccine_id', 2)->count();
+        $astra_first = $vaccinations->where('dose','first')->where('vaccine_id', 3)->count();
+        $astra_second = $vaccinations->where('dose','second')->where('vaccine_id', 3)->count();
+        $sinopharm_first = $vaccinations->where('dose','first')->where('vaccine_id', 5)->count();
+        $sinopharm_second = $vaccinations->where('dose','second')->where('vaccine_id', 5)->count();
+        $covid_first = $vaccinations->where('dose','first')->where('vaccine_id', 6)->count();
+        $covid_second = $vaccinations->where('dose','second')->where('vaccine_id', 6)->count();
+
 
         return view('dashboard', compact(
             'vaccinations', 'total_eligible','first_dose','second_dose','booster_dose', 'first_dose_percentage','second_dose_percentage','booster_dose_percentage',
---
+++ config/defaults.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return [
+
+    'total_eligible' => 976,
+];
+
